d17c51da9a402b356649a03a85f3affa589c668a
André Muth <andre.muth@tutanota.com>
2024-02-01 22:05:30 +0100

    Tested version 1.1 LinkedIn_Crawler (Selenium) for profiles and posts

--- LinkedIn_Crawler_Sel.py
+++ LinkedIn_Crawler_Sel.py
@@ -21,7 +21,7 @@ from datetime import datetime, timedelta
 import os
 # Settings
 chromedriver_path = r"C:\Users\andre\Documents\Python\chromedriver-win64\chromedriver.exe"
-path_to_crawler_functions = r"C:\Users\andre\Documents\Python\Web_Scraper\Social_Media_Crawler_2023"
+path_to_crawler_functions = r"C:\Users\andre\Documents\Python\Web_Crawler\Social_Media_Crawler_2024"
 file_path = r"C:\Users\andre\OneDrive\Desktop\Nahrungsergaenzungsmittel"
 source_file = "Liste_Nahrungsergänzungsmittel_2024_Auswahl.xlsx"
 branch_keywords = ['nutrition', 'vitamin', 'mineral', 'protein', 'supplement', 'diet', 'health', 'ernährung',
@@ -47,7 +47,6 @@ def login(username, password):
         time.sleep(.1)
     driver.find_element(By.XPATH, '//button[contains(text(), "Einloggen")]').click()
     time.sleep(2)
-
 
 def scrapeProfile(company, link):
     p_name, follower, employees, last_post, desc1, desc2, tagline = ['' for _ in range(7)]
@@ -190,43 +189,134 @@ def scroll_to_bottom():
         time.sleep(1)
         new_height = driver.execute_script('return document.body.scrollHeight')
         safety_counter += 1
+
+def check_conditions(id, p_name, url, row, lower_dt, start_at=0):
+    if id < start_at:      # If you want to skip some rows
+        return False
+    if len(p_name) == 0 or p_name.lower() == 'nan' or p_name == 'None':
+        return False
+    posts = str(row['last_post'])
+    if len(url) < 10 or len(posts) <= 4 or 'Keine Beiträge' in posts:
+        print([id, p_name, url])
+        return False
+    try:
+        last_datestr = extract_text(row['last_post'])
+        last_dt = datetime.strptime(last_datestr, "%d.%m.%Y")
+        if (last_dt + timedelta(days=31)) < lower_dt:
+            return False
+        driver.get(url + 'posts/?feedView=all')
+        time.sleep(2)
+        return True
+    except:
+        return False
+
+def scrape_post(p):
+    post_date_dt = datetime.now()
+    post_date = post_date_dt.strftime("%d.%m.%Y")
+    link_elems = [a for a in p.find_all('a',href=True)]
+    header_desc = [str(get_visible_text(Comment,a)).strip() for a in link_elems]
+    date_elements = ['Std', 'Tag', 'Woche', 'Monat', 'Jahr']
+    date_opt = [d for d in header_desc if any(e in d for e in date_elements)]
+    if len(date_opt) > 0:
+        date_str = date_opt[0]
+        if '•' in date_str:
+            date_str = date_str.split('•')[1].strip()
+        try:
+            post_date_dt, post_date = get_approx_date(post_date_dt, date_str)
+        except:
+            pass
+
+    likes, comments, shares = ['' for _ in range(3)]
+    react_elements = p.find_all('button', attrs={'aria-label': True})
+    aria_labels = [a['aria-label'] for a in react_elements]
+    for a in aria_labels:
+        ptext = str(extract_text(a))
+        num = extract_every_number(a)
+        if 'Reaktionen' in ptext and likes == '':
+            likes = num
+        elif ' Kommentar' in a and comments == '':
+            comments = num
+        elif ' geteilt' in a or 'Veröffentlichungen' in a and shares == '':
+            shares = num
+    content_elem = p.find('span', class_='break-words')
+    if content_elem:
+        content = get_visible_text(Comment, content_elem)
+    if not content_elem or len(str(content)) <= 4:
+        content = get_visible_text(Comment, p)
+
+    imagelinks = [e['src'] for e in p.find_all('img', src=True) if not 'company-logo' in e['src']]
+    if p.find('video'):
+        video, image = 1,0
+    elif len(imagelinks) >= 1 or p.find('ul', class_='carousel-track') or p.find('iframe', src=True):
+        image, video = 1,0
+    else:
+        image, video = 0,0
+    link = ''
+    result = [post_date, likes, comments, shares, image, video, link, content]
+    return post_date_dt, result
+########################################################################################################################
+
+# Post Crawler
+if __name__ == '__main__':
+    # Settings for the post crawler
+    os.chdir(path_to_crawler_functions)
+    from crawler_functions import *
+    import credentials_file as cred
+    os.chdir(file_path)
+    file ='Profile_LinkedIn_2024'
+    df_source, dt, dt_str, upper_dt, lower_dt = post_crawler_settings(file, platform, dt_str_now)
+
+    # Current date
+    upper_dt = datetime.now()
+
+    # Driver and Browser setup
+    all_data = []
+    driver = start_browser(webdriver, Service, chromedriver_path)
+    go_to_page(driver, startpage)
+    login(cred.useremail_li, cred.password_li)
+
+    # Iterate over the companies
+    for n, row in df_source.iterrows():
+        id = row['ID']
+        url = str(row['url'])
+        p_name = str(row['profile_name'])
+        go_crawl = check_conditions(n, p_name, url, row, lower_dt, start_at=27) # Start at the row 0
+        if not go_crawl:
+            continue
+        scroll_to_bottom()
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+        posts = soup.find_all('div', class_='ember-view occludable-update')
+        if len(posts) == 0:
+            continue
+
+        data_per_company = []
+        id_p = 0
+        for count, p in enumerate(posts):
+            post_dt, postdata = scrape_post(p)
+            print(post_dt, postdata)
+            if post_dt >= upper_dt:
+                continue
+            if post_dt < lower_dt:
+                break
+            id_p += 1
+            full_row = [id, p_name, id_p, dt_str] + postdata
+            data_per_company.append(full_row)
+
+        all_data += data_per_company
+
+        # Create a DataFrame with all posts
+        header1 = ['ID_A', 'Profilname', 'ID_P', 'Datum_Erhebung', 'Datum_Beitrag']
+        header2 = ['Likes', 'Kommentare', 'Shares', 'Bild', 'Video', 'Link', 'Content']
+        dfPosts = pd.DataFrame(all_data, columns=header1 + header2)
+
+        # Export dfPosts to Excel (with the current time)
+        dt_str_now = datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
+        file_name = 'Beiträge_' + platform + dt_str_now + '.xlsx'
+        dfPosts.to_excel(file_name)
+
+    driver.quit()
 ########################################################################################################################
 '''
-# Settings for the post crawler
-source_file = "Profildaten_Energieanbieter_1.xlsx"
-df_source = pd.read_excel(source_file, sheet_name=platform)
-df_source.set_index('ID',inplace=True)
-dt = datetime.now()
-dt_str = dt.strftime("%d.%m.%Y")
-lower_dt = datetime.strptime('2022-10-31','%Y-%m-%d')
-upper_dt = datetime.strptime('2023-11-01','%Y-%m-%d')
-
-data_posts = []
-
-new_path = r"C:\Users\andre\OneDrive\Desktop\SSM_Energieanbieter"
-os.chdir(new_path)
-datelimit_str = '31.10.2022'
-datelimit = datetime.strptime(datelimit_str,'%d.%m.%Y')
-
-
-# Start crawling
-driver = start_browser(webdriver, Service, chromedriver_path)
-go_to_page(driver, startpage)
-login(cred.useremail_li, cred.password_li)
-
-# Loop
-for id, row in df_source.iterrows():
-    if len(str(row['last post'])) <= 4 or str(row['last post']).strip() == 'Keine Beiträge':
-        continue
-    p_name = row['Profilname']
-    url = row['url']
-    print(id, p_name, url)
-
-#def scrape_posts(p_name, url):
-    driver.get(url + 'posts/?feedView=all')
-    time.sleep(2)
-    scroll_to_bottom()
-
 # def scrape_links():
     linklist = []
     dropdown_buttons = driver.find_elements(By.CSS_SELECTOR,'svg[a11y-text="Kontrollmenü öffnen"]')
@@ -246,18 +336,7 @@ for id, row in df_source.iterrows():
         copy_link_button = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Link zum Artikel kopieren')]"))
         )
-
     # Selenium is unable to locate the Button responsible for copying the link
-
-# This code ends here
-
-
-########################################################################################################################
-#if needed:
-if driver.current_url == startpage:
-    firstname = driver.find_element(By.CSS_SELECTOR, 'input#first-name')
-    lastname = driver.find_element(By.CSS_SELECTOR, 'input#last-name')
-    firstname.send_keys(firstname_li)
-    lastname.send_keys(lastname_li)
-    driver.find_element(By.XPATH, '//button[contains(text(), "Weiter")]').click()
 '''
+# header_desc = [str(a['aria-label']).strip() for a in p.find_all('a', {'aria-label': True})]
+# header_desc = [a.text for a in p.find_all('a', {'aria-label': True})]
