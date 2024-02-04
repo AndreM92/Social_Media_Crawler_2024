0951b7c27921817429bb092242df6b2fa5a42e7c
André Muth <andre.muth@tutanota.com>
2024-02-04 11:19:53 +0100

    Merging, summation, calculation and export of post data

--- aggregate_analyse_data.py
+++ aggregate_analyse_data.py
@@ -9,21 +9,21 @@ from langdetect import detect
 
 
 def get_tables(platform):
-    filename_posts, filename_profile = None, None
+    file_keys = ['Auswahl_', 'Profile_', 'Beiträge_']
+    file_names = ['' for _ in range(3)]
+    dfs = ['' for _ in range(len(file_names))]
     for f in os.listdir():
-        if 'Beiträge_' + platform in f:
-            filename_posts = f
-        elif 'Profile_' + platform in f:
-            filename_profile = f
-    if not filename_profile:
-        print('Profile_' + platform + ' not in path')
-        exit()
-    if not filename_posts:
-        print('Beiträge_' + platform + ' not in path')
-        exit()
-    df_profiles = pd.read_excel(filename_profile)
-    df_posts = pd.read_excel(filename_posts)
-    return df_profiles, df_posts
+        if file_names[0] == '' and file_keys[0] in f:
+            file_names[0] = f
+        for id, n in enumerate(file_keys):
+            if file_names[id] == '' and n + platform in f:
+                file_names[id] = f
+    for id, f in enumerate(file_names):
+        if not f:
+            print(file_keys[id] + platform + ' not in path')
+            exit()
+        dfs[id] = pd.read_excel(f)
+    return dfs
 
 
 if __name__ == '__main__':
@@ -32,54 +32,72 @@ if __name__ == '__main__':
     platforms = ['LinkedIn']
 
     for platform in platforms:
-        df_profiles, df_posts = get_tables(platform)
-        # Get the first table with the selections and firm names also
+        df_selection, df_profiles, df_posts = get_tables(platform)
+        df_profiles.rename(columns={'ID': 'ID_A', 'company': 'Name in Studie', 'profile_name': 'Profilname',
+                                    'url': platform}, inplace=True)
 
-        if 'description1' in df_profiles.columns and 'description2' in df_profiles.columns:
-            df_profiles['description'] = (df_profiles['description1'].fillna('') + ' ' + \
-                                         df_profiles['description2'].fillna('')).str.strip()
-        if 'tagline' in df_profiles.columns:
-            df_profiles['description'] = (df_profiles['tagline'].fillna('') + ' ' +
-                                          df_profiles['description'].fillna('')).str.strip()
+        # Create a DF with selected Profiles
+        df_sel_profiles = df_selection[['ID_new', 'ID_A', 'Firma/Marke']].merge(df_profiles, on='ID_A', how='left')
+        columns_to_remove = ['Unnamed', 'Number', 'date', 'employees']
+        for c in df_sel_profiles.columns:
+            for cn in columns_to_remove:
+                if cn in c:
+                    df_sel_profiles.drop(columns=c, inplace=True)
+        desc, d1, d2, tl = 'description', 'description1', 'description2', 'tagline'
+        if d1 in df_sel_profiles.columns and d2 in df_sel_profiles.columns:
+            df_sel_profiles[desc] = (df_sel_profiles[d1].fillna('') + ' ' +
+                                              df_sel_profiles[d1].fillna('')).str.strip()
+            df_sel_profiles.drop(columns={d1,d2}, inplace=True)
+        if tl in df_sel_profiles.columns:
+            df_sel_profiles[desc] = (df_sel_profiles[tl].fillna('') + ' ' +
+                                          df_sel_profiles[desc].fillna('')).str.strip()
+            df_sel_profiles.drop(columns={tl}, inplace=True)
+        if not 'follower' in df_profiles.columns:
+            print('Missing column in ' + platform)
+            exit()
+        if not 'fans' in df_profiles.columns:
+            df_sel_profiles.rename(columns={'follower': 'fans'}, inplace=True)
+        df_sel_profiles.rename(columns={'fans':'Fans', 'description':'Beschreibung'},inplace=True)
 
-        if 'fans' in df_profiles.columns:
-            df_profiles_red = df_profiles[['ID', 'company', 'profile_name', 'fans', 'url', 'description']].copy()
+        # Create a DF with selected Posts
+        df_sel_posts = df_sel_profiles[['ID_new', 'ID_A', 'Name in Studie', 'Fans']].merge(df_posts, on='ID_A', how='inner')
+        columns_to_remove = ['Unnamed', 'Number', 'Erhebung']
+        for c in df_sel_posts.columns:
+            for cn in columns_to_remove:
+                if cn in c:
+                    df_sel_posts.drop(columns=c, inplace=True)
+        if 'Shares' in df_sel_posts.columns:
+            df_sel_posts['Interaktionen'] = df_sel_posts[['Likes', 'Kommentare', 'Shares']].sum(axis=1)
+        elif 'Retweets' in df_sel_posts.columns:
+            df_sel_posts['Interaktionen'] = df_sel_posts[['Likes', 'Kommentare', 'Retweets']].sum(axis=1)
         else:
-            df_profiles_red = df_profiles[['ID', 'company', 'profile_name', 'follower', 'url', 'description']].copy()
-            df_profiles_red.rename(columns={'follower':'fans'},inplace=True)
-        df_profiles_red.rename(columns={'ID': 'ID_A', 'company': 'Name in Studie', 'profile_name': 'Profilname',
-                                        'fans':'Fans', 'description':'Beschreibung'},inplace=True)
-
-        df_agg = pd.merge(df_posts, df_profiles_red[['ID_A', 'Fans']], on='ID_A', how='inner')
-        if 'Shares' in df_agg.columns:
-            df_agg['Interaktionen'] = df_agg[['Likes', 'Kommentare', 'Shares']].sum(axis=1)
-        elif 'Retweets' in df_agg.columns:
-            df_agg['Interaktionen'] = df_agg[['Likes', 'Kommentare', 'Retweets']].sum(axis=1)
-        else:
-            df_agg['Interaktionen'] = df_agg[['Likes', 'Kommentare']].sum(axis=1)
+            df_sel_posts['Interaktionen'] = df_sel_posts[['Likes', 'Kommentare']].sum(axis=1)
 
         # Calculations of post numbers
-        if 'Shares' in df_agg.columns:
-            df_posts_calc = df_agg.groupby('ID_A').agg(
+        if 'Shares' in df_sel_posts.columns:
+            df_agg = df_sel_posts.groupby('ID_new').agg(
                 {'ID_A': 'size', 'Likes': 'sum', 'Kommentare': 'sum', 'Shares': 'sum', 'Interaktionen': 'sum'}
             ).rename(columns={'ID_A': 'Anzahl Posts', 'Likes': 'Anzahl Likes', 'Kommentare': 'Anzahl Kommentare',
                 'Shares': 'Anzahl Shares', 'Interaktionen': 'Anzahl Interaktionen'})
-        elif 'Retweets' in df_agg.columns:
-            df_posts_retweets = df_agg[df_posts['Beitragsart'] == 'retweet']
+        elif 'Retweets' in df_sel_posts.columns:
+            df_posts_retweets = df_sel_posts[df_sel_posts['Beitragsart'] == 'retweet']
             count_retweets = df_posts_retweets.groupby('ID_A').size()
             count_retweets.name = 'Retweet Posts'
-            df_agg = df_posts[df_posts['Beitragsart'] != 'retweet']
-            df_posts_calc = df_posts.groupby('ID_A').agg(
+            df_sel_posts = df_sel_posts[df_sel_posts['Beitragsart'] != 'retweet']
+            df_agg = df_sel_posts.groupby('ID_new').agg(
                 {'ID_A': 'size', 'Likes': 'sum', 'Kommentare': 'sum', 'Retweets': 'sum','Interaktionen': 'sum'}
             ).rename(columns={'ID_A': 'Anzahl Posts', 'Likes': 'Anzahl Likes', 'Kommentare': 'Anzahl Kommentare',
                 'Retweets': 'Anzahl Retweets', 'Interaktionen': 'Anzahl Interaktionen'})
         else:
-            df_posts_calc = df_agg.groupby('ID_A').agg({'ID_A': 'size', 'Likes': 'sum', 'Kommentare': 'sum'}).rename(
+            df_agg = df_sel_posts.groupby('ID_new').agg({'ID_A': 'size', 'Likes': 'sum', 'Kommentare': 'sum'}).rename(
                 columns={'ID_A': 'Anzahl Posts', 'Likes': 'Anzahl Likes', 'Kommentare': 'Anzahl Kommentare'})
-            df_posts_calc['Interaktionen'] = df_posts_calc[['Anzahl Likes', 'Anzahl Kommentare']].sum(axis=1)
+            df_agg['Interaktionen'] = df_agg[['Anzahl Likes', 'Anzahl Kommentare']].sum(axis=1)
 
-        df_agg.fillna(0, inplace=True)
+        # Merge dfs
+        df_sel_posts = df_sel_posts.merge(df_agg, on='ID_new', how='inner')
 
+        # Calculate the Interaction rate for each post
+        df_sel_posts['Interaktionsrate'] = (df_sel_posts['Interaktionen'] /
+                                            (df_sel_posts['Fans'] * df_sel_posts['Anzahl Posts']) * 1000)
 
-
-
+        df_sel_posts.to_excel('Postdata_agg_' + platform + '.xlsx')
