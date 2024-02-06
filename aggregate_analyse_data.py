e412c12edb8ba1e7f30e404b33374160a0c8c7be
André Muth <andre.muth@tutanota.com>
2024-02-06 09:11:52 +0100

    Concatenation of the dfs and export of the channels, posts and rankings to excel

--- aggregate_analyse_data.py
+++ aggregate_analyse_data.py
@@ -34,7 +34,7 @@ dict_ranking = {}
 if __name__ == '__main__':
     file_path = r"C:\Users\andre\OneDrive\Desktop\Nahrungsergaenzungsmittel"
     os.chdir(file_path)
-    platforms = ['LinkedIn']
+    platforms = ['Facebook', 'Instagram', 'LinkedIn', 'TikTok', 'Twitter', 'YouTube']
 
     for platform in platforms:
         df_selection, df_profiles, df_posts = get_tables(platform)
@@ -50,7 +50,7 @@ if __name__ == '__main__':
         desc, d1, d2, tl = 'description', 'description1', 'description2', 'tagline'
         if d1 in df_sel_profiles.columns and d2 in df_sel_profiles.columns:
             df_sel_profiles[desc] = (df_sel_profiles[d1].fillna('') + ' ' +
-                                              df_sel_profiles[d1].fillna('')).str.strip()
+                                              df_sel_profiles[d2].fillna('')).str.strip()
             df_sel_profiles.drop(columns={d1,d2}, inplace=True)
         if tl in df_sel_profiles.columns:
             df_sel_profiles[desc] = (df_sel_profiles[tl].fillna('') + ' ' +
@@ -86,9 +86,9 @@ if __name__ == '__main__':
                 'Shares': 'Anzahl Shares', 'Interaktionen': 'Anzahl Interaktionen'})
         elif 'Retweets' in df_sel_posts.columns:
             df_posts_retweets = df_sel_posts[df_sel_posts['Beitragsart'] == 'retweet']
-            count_retweets = df_posts_retweets.groupby('ID_A').size()
+            count_retweets = df_posts_retweets.groupby('ID_new').size()
             count_retweets.name = 'Retweet Posts'
-            df_agg = df_agg.merge(count_retweets, on='ID_A', how='left')
+            df_agg = df_agg.merge(count_retweets, on='ID_new', how='left')
             df_sel_posts = df_sel_posts[df_sel_posts['Beitragsart'] != 'retweet']
             df_agg = df_sel_posts.groupby('ID_new').agg(
                 {'ID_A': 'size', 'Likes': 'sum', 'Kommentare': 'sum', 'Retweets': 'sum','Interaktionen': 'sum'}
@@ -97,8 +97,7 @@ if __name__ == '__main__':
         else:
             df_agg = df_sel_posts.groupby('ID_new').agg({'ID_A': 'size', 'Likes': 'sum', 'Kommentare': 'sum'}).rename(
                 columns={'ID_A': 'Anzahl Posts', 'Likes': 'Anzahl Likes', 'Kommentare': 'Anzahl Kommentare'})
-            df_agg['Interaktionen'] = df_agg[['Anzahl Likes', 'Anzahl Kommentare']].sum(axis=1)
-
+            df_agg['Anzahl Interaktionen'] = df_agg[['Anzahl Likes', 'Anzahl Kommentare']].sum(axis=1)
 
         interactions_above_zero = df_sel_posts[df_sel_posts['Interaktionen'] >= 1].groupby('ID_new').size()
         interactions_above_zero.name = 'Interaktionen>0'
@@ -148,7 +147,7 @@ if __name__ == '__main__':
 
         # A short summary of the profiles and their posting activity
         df_posts_compact = df_profiles_summary[['ID_new', 'ID_A', 'Name in Studie', 'Anzahl Posts', 'Link']].copy()
-        df_post_compact.rename(columns={'Name in Studie': 'Anbieter', 'Anzahl Posts': platform + '_posts',
+        df_posts_compact.rename(columns={'Name in Studie': 'Anbieter', 'Anzahl Posts': platform + '_posts',
                                         'Link': platform}, inplace=True)
 
         # Calculate the Interaction rate for each post
@@ -179,10 +178,81 @@ if __name__ == '__main__':
         dict_postinfo[platform] = df_posts_compact
         dict_posts[platform] = df_sel_posts
         dict_ranking[platform] = df_ranking_s
+        print(platform + ' Done')
+
+    ########################################################################################################################
+    # Concat/ Merge the full tables
+
+    # Complete profile data
+    table_profiles = dict_summary[platforms[0]]
+    for p, df in dict_summary.items():
+        if p == platforms[0]:
+            continue
+        table_profiles = pd.concat([df_all_profiles_sum, df], axis=0)
+
+    # Custom function for na-filling
+    table_profiles['Link'].fillna('-', inplace=True)
+
+    def custom_fill(row):
+        if len(str(row['Link'])) < 5:
+            return row.fillna('-')
+        elif pd.isna(row['Anzahl Posts']):
+            row['Anzahl Posts'] = 0
+        return row.fillna('-')
+
+    # Apply the custom function to each row
+    table_profiles = table_profiles.apply(custom_fill, axis=1)
+    table_profiles.columns.values[0]
 
 
+    # Summarize the posting activity
+    table_postinfo = dict_postinfo[platforms[0]][['ID_new', 'Anbieter']]
+    for p, df in dict_postinfo.items():
+        table_postinfo = table_postinfo.merge(df[['ID_new', p + '_posts', p]], on='ID_new', how='left')
+    table_postinfo['Anzahl Kanäle'] = table_postinfo[platforms].apply(lambda row: sum('http' in str(val) for val in row), axis=1)
+    n_posts = [p + '_posts' for p in platforms]
+    table_postinfo['Aktiv genutzte Kanäle'] = table_postinfo[n_posts].apply(
+        lambda row: sum(~row.astype(str).isna() & (pd.to_numeric(row, errors='coerce') > 0)), axis=1)
+    table_postinfo['Beiträge gesamt'] = table_postinfo[n_posts].sum(axis=1)
+    col_order = ['ID_new', 'Anbieter', 'Anzahl Kanäle', 'Aktiv genutzte Kanäle', 'Beiträge gesamt'] + n_posts + platforms
+    table_postinfo = table_postinfo[col_order]
+
+    dict_posts['Übersicht'] = table_postinfo
 
 
+    # Final Ranking table
+    table_ranking = table_postinfo[['ID_new', 'Anbieter', 'Anzahl Kanäle', 'Aktiv genutzte Kanäle']].copy()
+    for platform, df in dict_ranking.items():
+        if platform == 'Twitter':
+            platform = 'X'
+        df_ranking_i = df[['ID_new', 'Rang', 'Index']].copy()
+        df_ranking_i.rename(columns={'Rang': 'Rang_' + platform[:1], 'Index': 'Index_' + platform[:1]}, inplace=True)
+        table_ranking = table_ranking.merge(df_ranking_i, on='ID_new', how='left')
+
+    table_ranking['Rang_avg'] = (table_ranking[['Rang_F', 'Rang_I', 'Rang_L',
+                                                'Rang_T', 'Rang_X', 'Rang_Y']].mean(axis=1)).round(2)
+    table_ranking['Index_avg'] = (table_ranking[['Index_F', 'Index_I', 'Index_L',
+                                                 'Index_T', 'Index_X', 'Index_Y']].mean(axis=1)).round(2)
+    table_ranking['Rang'] = table_ranking['Index_avg'].rank(ascending=False, na_option='bottom', method='dense')
+    # table_ranking['Punkte'] = (table_ranking['Index_avg'] - table_ranking['Rang_avg']**0.5).round(2)
+    # table_ranking['Rang'] = table_ranking['Punkte'].rank(ascending=False, na_option='bottom', method='dense')
+    col_order_rf = ['Rang'] + list(table_ranking.columns)[:-1]
+    table_ranking = table_ranking[col_order_rf]
+    table_ranking = table_ranking.sort_values(by='Rang').reset_index(drop=True)
+    dict_ranking['Gesamtranking'] = table_ranking
 
 
-        #df_sel_posts.to_excel('Postdata_agg_' + platform + '.xlsx')
+    # Export to excel
+    date_str = datetime.now().strftime("%Y%m%d")
+    name_profiles = 'Kanäle_Supplements ' + date_str + '.xlsx'
+    name_posts = 'Beiträge_Supplements ' + date_str + '.xlsx'
+    name_ranks = 'Ranking_Supplements ' + date_str + '.xlsx'
+
+    table_profiles.to_excel(name_profiles)
+
+    with pd.ExcelWriter(name_ranks, engine='xlsxwriter') as writer:
+        for platform, df in dict_ranking.items():
+            df.to_excel(writer, sheet_name=platform)
+    with pd.ExcelWriter(name_posts, engine='xlsxwriter') as writer:
+        for platform, df in dict_posts.items():
+            df.to_excel(writer, sheet_name=platform)
