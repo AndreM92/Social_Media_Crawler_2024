bcacbc1bc5263f1cd9bbc7f124d420a83a9e3155
André Muth <andre.muth@tutanota.com>
2025-02-20 18:28:15 +0100

    update to changed HTML

--- YouTube_Crawler_Sel.py
+++ YouTube_Crawler_Sel.py
@@ -1,3 +1,4 @@
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -23,19 +24,26 @@ path_to_crawler_functions = r"C:\Users\andre\Documents\Python\Web_Crawler\Social
 startpage = 'https://www.youtube.com/'
 platform = 'YouTube'
 dt_str_now = None
-upper_datelimit = '2024-08-31'
+upper_datelimit = '2025-02-01'
 
-file_path = r"C:\Users\andre\OneDrive\Desktop\SMP_Krankenkassen_2024"
-source_file = r"C:\Users\andre\OneDrive\Desktop\SMP_Krankenkassen_2024\Auswahl_Krankenkassen.xlsx"
-branch_keywords = ['Kranken', 'Gesundheit', 'Leistung']
+file_path = r"C:\Users\andre\OneDrive\Desktop\SMP_Arzneimittelhersteller_2025"
+source_file = file_path + '\Auswahl_Arzneimittelhersteller 2025_2025-02-05.xlsx'
+branch_keywords = ['Pharma', 'Arznei', 'Medikament', 'Wirkstoff', 'Supplement', 'Forschung', 'Studie', 'Medizin', 'leistung', 'Krank', 'krank']
 #branch_keywords = ['nutrition', 'vitamin', 'mineral', 'protein', 'supplement', 'diet', 'health', 'ernährung',
 #                   'ergänzung', 'gesundheit', 'nährstoff', 'fitness', 'sport', 'leistung']
 #file_path = r"C:\Users\andre\OneDrive\Desktop\Nahrungsergaenzungsmittel"
 #source_file = "Liste_Nahrungsergänzungsmittel_2024_Auswahl.xlsx"
 ########################################################################################################################
 
-def crawlVideo(link):
-    date, title, views, likes, comments, desc = ['' for _ in range(6)]
+def base_url(url):
+    to_remove = ['/videos', '/about', '/featured', '/playlists']
+    for e in to_remove:
+        url = url.replace(e,'')
+    if url[-1] == '/':
+        url = url[:-1]
+    return url
+
+def open_and_check_page(link):
     try:
         driver.get(link)
         time.sleep(3)
@@ -52,8 +60,9 @@ def crawlVideo(link):
     status_code = requests.head(link).status_code
     if status_code != 200:
         return False
+    return True
 
-    link = driver.current_url
+def open_description(driver):
     expand = driver.find_elements(By.CSS_SELECTOR, 'tp-yt-paper-button[id*="expand"]')
     if len(expand) > 1:
         for e in expand:
@@ -63,22 +72,24 @@ def crawlVideo(link):
                 pass
     elif len(expand) == 1:
         expand.click()
-    driver.execute_script("window.scrollBy(0,1000)", "")
-    time.sleep(2)
-    soup = BeautifulSoup(driver.page_source,'html.parser')
-    pagetext = extract_text(soup)
+
+def get_likes(soup):
+    likes = ''
     like_elem = soup.find('like-button-view-model')
     if like_elem:
         likes = extract_every_number((extract_text(like_elem)))
-        if likes == 'Mag ich':
-            likes = ''
-            like_elem = ''
-    if not like_elem:
-        like_elem = soup.find('button', {'aria-label': lambda x: x and 'mag das Video' in x})
-        if like_elem:
-            like_text = extract_text(like_elem['aria-label'])
-            if 'mag' in like_text.lower():
-                likes = extract_number(like_text)
+        if not like_elem or 'Mag' in str(likes):
+            like_elem = soup.find('button', {'aria-label': lambda x: x and 'mag das Video' in x})
+            if like_elem:
+                like_text = extract_text(like_elem['aria-label'])
+                if 'mag' in like_text.lower():
+                    likes = extract_number(like_text)
+    if likes == 'Mag ich':
+        likes = ''
+    return likes
+
+def get_video_title(soup):
+    title = ''
     title_elem = soup.find('h1',class_='style-scope ytd-watch-metadata')
     title = extract_text(title_elem)
     if len(str(title)) <= 4:
@@ -88,9 +99,12 @@ def crawlVideo(link):
             if len(str(t_text)) >= 4:
                 title = t_text
                 break
+    return title
+
+def get_video_details(soup):
+    date_str, date, views, desc = ['' for _ in range(4)]
     desc_elem = soup.find('div',{'id':'bottom-row'})
     full_desc = extract_text(desc_elem)
-    date_str = ''
     if full_desc:
         desc = full_desc
         if 'Weniger anzeigen' in desc:
@@ -110,9 +124,12 @@ def crawlVideo(link):
                     pass
             if views != '' and date != '':
                 break
-    if 'Kommentare sind deaktiviert' in pagetext:
-        comments = ''
-        return [date, title, views, likes, comments, link, desc]
+    if desc == '…':
+        desc = ''
+    return [date_str, date, views, desc]
+
+def get_comment_number(driver,soup):
+    comments = ''
     comments_elem = soup.find('h2',{'id':'count'})
     if comments_elem:
         comments_text = extract_text(comments_elem)
@@ -128,49 +145,60 @@ def crawlVideo(link):
                 comments_text = extract_text(comments_elem)
                 if 'Kommentar' in comments_text or 'comment' in comments_text:
                     comments = extract_number(comments_elem)
-            if comments != '':
-                break
-        if comments == '':
+                    if comments != '':
+                        break
+        if not comments and str(comments) != '0':
             try:
                 comments_elem = driver.find_element(By.CLASS_NAME, 'style-scope ytd-comments-header-renderer').text
                 if 'Kommentar' in comments:
                     comments = extract_number(comments_elem)
             except:
                 pass
+    return comments
+
+
+def crawl_video(driver, link):
+    date, title, views, likes, comments, desc = ['' for _ in range(6)]
+    page_status = open_and_check_page(link)
+    if not page_status:
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        pagetext = extract_text(soup)
+        return [pagetext] + ['' for _ in range(6)]
+    link = driver.current_url
+    open_description(driver)
+    driver.execute_script("window.scrollBy(0,1000)", "")
+    time.sleep(2)
+    soup = BeautifulSoup(driver.page_source,'html.parser')
+    pagetext = extract_text(soup)
+    likes = get_likes(soup)
+    title = get_video_title(soup)
+    date_str, date, views, desc = get_video_details(soup)
+    if '0 Kommentare' in pagetext:
+        comments = 0
+    if not 'Kommentare sind deaktiviert' in pagetext and not str(comments) == '0':
+        comments = get_comment_number(driver, soup)
     return [date, title, views, likes, comments, link, desc]
 
-
 # A function to open the targetpage and scrape the profile stats
-def scrapeProfile(url):
-    p_name, follower, total_posts, last_post, v_link, desc = ['' for i in range(6)]
-    transform_url = ['/videos', '/about', '/featured', '/playlists']
-    for e in transform_url:
-        url = url.replace(e,'')
-    if url[-1] == '/':
-        url = url[:-1]
+def scrapeProfile(url, comp_keywords):
+    p_name, follower, total_posts, last_post, link, desc = ['' for _ in range(6)]
+    url = base_url(url)
     driver.get(url + '/videos')
     time.sleep(3)
-    new_url = driver.current_url.replace('/videos','')
     soup = BeautifulSoup(driver.page_source, 'lxml')
     pagetext = extract_text(get_visible_text(Comment, soup))
     restricted = ['potenziell ungeeignet für', 'dass du alt genug', 'leider nicht verfügbar']
     if any(e in pagetext for e in restricted) or len(pagetext) <= 1000:
-        return [p_name, follower, total_posts, last_post, new_url, pagetext]
-    p_name = extract_text(soup.find('ytd-channel-name',{'id':'channel-name'}))
-    if p_name and len(p_name) >= 3 and ' ' in p_name:
-        p_name1 = p_name[:round((len(p_name)/2))].strip()
-        p_name2 = p_name[round((len(p_name)/2)):].strip()
-        if p_name1[:-1] in p_name2:
-            p_name = p_name1
-    if not p_name or len(p_name)<=4:
-        headers = [extract_text(e) for e in soup.find_all('h1')]
-        for h in headers:
-            if len(h) > 4:
-                p_name = h
+        return [p_name, follower, total_posts, last_post, url, pagetext]
+#    p_name = extract_text(soup.find('ytd-channel-name',{'id':'channel-name'}))
+    name_options = soup.find_all('h1')
+    for n in name_options:
+        n_text = extract_text(n)
+        if any(c.lower() in n_text.lower() for c in comp_keywords):
+            p_name = n_text
+            if len(p_name):
                 break
-    follower_elem = extract_text(soup.find('yt-formatted-string',{'id':'subscriber-count'}))
-    if follower_elem and 'abonnent' in follower_elem.lower():
-        follower = extract_every_number(follower_elem)
+#    follower_elem = extract_text(soup.find('yt-formatted-string',{'id':'subscriber-count'}))
     span_text = [extract_text(e) for e in soup.find_all('span')]
     for s in span_text:
         if 'Abonnenten' in s and not follower:
@@ -183,8 +211,8 @@ def scrapeProfile(url):
     if len(video_d) >= 1:
         videolinks = ['https://www.youtube.com' + v.find('a', href=True)['href'] for v in video_d if v.find('a', href=True)]
         if len(videolinks) >= 1:
-            v_link = videolinks[0]
-    driver.get(new_url + '/about')
+            link = videolinks[0]
+    driver.get(url + '/about')
     time.sleep(3)
     if not '/about' in driver.current_url:
         try:
@@ -197,12 +225,11 @@ def scrapeProfile(url):
     desc_elem = soup.find('tp-yt-paper-dialog')
     if desc_elem:
         desc = get_visible_text(Comment, desc_elem).replace('Kanalinfo','').strip()
-
     # Get the date of the latest video
-    if len(v_link) >= 10:
-        result_row = crawlVideo(v_link)
+    if len(link) >= 10:
+        result_row = crawl_video(driver, link)
         last_post = result_row[0]
-    return [p_name, follower, total_posts, last_post, new_url, desc]
+    return [p_name, follower, total_posts, last_post, url, desc]
 ########################################################################################################################
 
 # Profile crawler
@@ -220,10 +247,9 @@ if __name__ == '__main__':
     go_to_page(driver, startpage)
 
     # Iterating over the companies
-    count = 0  # If id's aren't ordered
-    for id, row in df_source.iterrows():
-        count += 1
-        if count <= 0:  # If you want to skip some rows
+    for n, row in df_source.iterrows():
+        id = row['ID']
+        if id < 0:  # If you want to skip some rows / if n < 0:
             continue
         company = extract_text(row[comp_header])
         comp_keywords = get_company_keywords(company, row, col_list)
@@ -233,10 +259,10 @@ if __name__ == '__main__':
             data.append(empty_row)
             continue
 
-        scraped_data = scrapeProfile(url)
+        scraped_data = scrapeProfile(url, comp_keywords)
         full_row = [id, company, dt_str] + scraped_data
         data.append(full_row)
-        print(count,full_row[:-1])
+        print(n, full_row[:-1])
 
 
     # DataFrame
@@ -318,7 +344,7 @@ def crawl_all_videos(dt_str, row, videolinks):
     data_per_company = []
     id_p = 0
     for link in videolinks:
-        scraped_data = crawlVideo(link)
+        scraped_data = crawl_video(driver, link)
         if not scraped_data:
             continue
         post_date = scraped_data[0]
@@ -333,7 +359,7 @@ def crawl_all_videos(dt_str, row, videolinks):
         id_p += 1
         full_row = [id, p_name, id_p, dt_str] + scraped_data
         data_per_company.append(full_row)
-#        print(full_row[:-1])
+        print(full_row)
     return data_per_company
 
 ########################################################################################################################
@@ -345,7 +371,7 @@ if __name__ == '__main__':
     from crawler_functions import *
     import credentials_file as cred
     os.chdir(file_path)
-    file ='Profile_' + platform + '_2024'
+    file ='Profile_' + platform + '_2025'
     df_source, dt, dt_str, upper_dt, lower_dt = post_crawler_settings(file, platform, dt_str_now, upper_datelimit)
 
     # Driver and Browser setup
@@ -356,8 +382,7 @@ if __name__ == '__main__':
     # Iterate over the companies
     for count, row in df_source.iterrows():
         url = str(row['url'])
-        print(count, url)
-        go_crawl = check_conditions(count,row,start_at=0)
+        go_crawl = check_conditions(count,row,start_at=82)
         if not go_crawl:
             continue
 
@@ -368,7 +393,6 @@ if __name__ == '__main__':
 
         data_per_company = crawl_all_videos(dt_str, row, videolinks)
         all_data += data_per_company
-
 
         # Create a DataFrame with all posts
         header1 = ['ID_A', 'profile_name', 'ID_P', 'Erhebung', 'Datum']
@@ -394,3 +418,14 @@ print(all_data[-1])
             go_to_page(driver, startpage)
             time.sleep(1)
 '''
+
+soup = BeautifulSoup(driver.page_source,'lxml')
+elements = soup.find_all('p',class_='small')
+distinct_companies = []
+for e in elements:
+    company = extract_text(e)
+    if company not in distinct_companies:
+        distinct_companies.append(company)
+
+for e in distinct_companies:
+    print(e)
